003fdd4bb2b4bc5ce90e1dc78b3e970f892ec4d4
Muhammad Alif Ramadhani <muhalif1604@gmail.com>
2026-09-15 11:28:51 +0700

    fix: restore test to pass CI

--- src/states/authUser/reducer.test.js
+++ src/states/authUser/reducer.test.js
@@ -62,6 +62,6 @@ describe('authUserReducer function', () => {
     const nextState = authUserReducer(initialState, action);
 
     // assert
-    expect(nextState).toBe(123);
+    expect(nextState).toBeNull();
   });
 });
